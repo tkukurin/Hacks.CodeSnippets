0b8413101a0bd354fe62ce18720832846d46d8da
Toni Kukurin <tkukurin@gmail.com>
2022-02-09 13:09:22 +0000

    "Failure" cases of memoization

--- joblib_memoization.py
+++ joblib_memoization.py
@@ -12,18 +12,31 @@ import logging
 import joblib
 
 logging.basicConfig(level=logging.DEBUG)
+L = logging.getLogger(__name__)
 
 mem = joblib.Memory("~/.cache")
+
+
+def dependency(x: str):
+  # NOTE: a change in this function will not affect the caching.
+  # Also note that external state (randomness) is not accounted for.
+  if len(x) < 10: return x
+  return f"{x}{random.randint(0, 10)}"
 
 
 @mem.cache
 def memoized(x: str):
   # Changing even a comment makes joblib recompute existing results.
   # It prints a warning in the log.
-  return x + " additional string"
+  L.info("Actual implementation called")
+  return x + " additional string " + dependency(x)
 
 
 if __name__ == "__main__":
-  print(memoized("this is my string new"))
-  print(memoized(f"this is my string {random.randint(0, 10_000)}"))
+  inp = "this is my string new"
+  L.info("Calling with string len %s", len(inp))
+  random.seed(111)
+  print(memoized(inp))
+  print(dependency(inp))
+  # print(memoized(f"this is my string {random.randint(0, 10_000)}"))
 
