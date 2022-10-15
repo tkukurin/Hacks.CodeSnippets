814e80f3315c52479c789b3d66c3d20f21e9c374
Toni Kukurin <tkukurin@gmail.com>
2022-10-15 08:42:49 +0000

    ssh

--- fish_bootstrap/setup.sh
+++ fish_bootstrap/setup.sh
@@ -29,6 +29,7 @@ apt_pkgs="\
 "
 log "Installing from apt:\n$apt_pkgs"
 
+sudo apt-get install software-properties-common
 sudo apt-add-repository --yes ppa:fish-shell/release-3
 sudo apt update
 sudo apt-get update
@@ -61,22 +62,17 @@ done
 
 GH=https://raw.githubusercontent.com
 
-# oh-my-fish / omf
-# curl $GH/oh-my-fish/oh-my-fish/master/bin/install \
-#   | argv='--noninteractive' fish
-# vs. fisher (will just ignore if already installed)
-source /dev/stdin <<< "$(curl https://git.io/fisher)"
-fish -c "fisher install jorgebucaran/fisher"
-fish -c "fisher install wfxr/forgit"
-fish -c "fisher install jorgebucaran/nvm.fish"
-
 # docker/docker-slim on ARM
 # https://www.docker.com/blog/getting-started-with-docker-for-arm-on-linux/
-log "Docker setup"
-curl -fsSL test.docker.com -o get-docker.sh && sh get-docker.sh && rm get-docker.sh
-sudo usermod -aG docker $USER
-curl -sL $GH/docker-slim/docker-slim/master/scripts/install-dockerslim.sh \
-  | sudo -E bash -
+if ! which docker; then
+  log "Docker setup"
+  curl -fsSL test.docker.com -o get-docker.sh && sh get-docker.sh && rm get-docker.sh
+  sudo usermod -aG docker $USER
+  curl -sL $GH/docker-slim/docker-slim/master/scripts/install-dockerslim.sh \
+    | sudo -E bash -
+else
+  log "Skipping Docker setup"
+fi
 # need to logout and back in
 
 # see some packages https://github.com/oh-my-fish/packages-main/tree/master/packages
@@ -93,8 +89,17 @@ fishcfg=~/.config/fish/config.fish
 fishfns=~/.config/fish/functions
 
 [[ -d "$fishfns" ]] \
-  && rmdir "$fishfns" \
+  && mv "$fishfns" "fishfn_backup" \
   && ln -s "$CURDIR/fishfn/" "$fishfns"
+
+# oh-my-fish / omf
+# curl $GH/oh-my-fish/oh-my-fish/master/bin/install \
+#   | argv='--noninteractive' fish
+# vs. fisher (will just ignore if already installed)
+fish -c "curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher"
+fish -c "fisher install edm/bass"
+fish -c "fisher install wfxr/forgit"
+fish -c "fisher install jorgebucaran/nvm.fish"
 
 touch $fishcfg
 if grep -q "START inserted by install script" "$fishcfg"; then
@@ -119,40 +124,48 @@ set -Ux forgit_fixup fgfu
 set -Ux forgit_checkout_commit fgco
 set -Ux forgit_revert_commit fgrc
 
-zoxide init fish | source
+zoxide init fish | source || echo "WARN: zoxide plugin fail?"
+fish_ssh_agent || echo "WARN: fish_ssh_agent missing?"
 
 # END inserted by install script }}}
 EOF
 fi
 
-condav=4.11.0
-condaarch=Linux-aarch64
-log "Installing conda ${condav} (${condaarch})"
+# don't have to ssh-add, cf.
+# https://github.com/ivakyb/fish_ssh_agent
+if ! grep -q "AddKeysToAgent" ~/.ssh/config; then
+  echo "AddKeysToAgent yes" >> ~/.ssh/config
+fi
 
-wget https://repo.anaconda.com/miniconda/Miniconda3-py39_${condav}-${condaarch}.sh | bash
-condabin=$HOME/miniconda3/bin
-[[ -d "$condabin" ]] \
-  || die "Expected conda bin not found: $condabin"
-log "Adding conda bin $condabin to path"
-export PATH="$condabin:$PATH"
-conda update -n base -y conda
-conda init fish
-conda init bash
-conda config --add channels conda-forge
-log "Installing mamba"
-# https://docs.conda.io/projects/conda/en/latest/user-guide/tasks/manage-channels.html#strict-channel-priority
-# conda config --set channel_priority false
-conda install -y conda-libmamba-solver
-conda install -y -c conda-forge mamba
-additional_install="\
-  jupyter \
-  jupyterlab \
-  nb_conda_kernels \
-  pdm\
-"
-log "Installing via mamba:\n$additional_install"
-mamba install -y -c conda-forge $additional_install
-
+if ! which conda; then
+  log "Conda install"
+  wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-aarch64.sh -O miniconda.sh &&
+    bash miniconda.sh && rm miniconda.sh
+  condabin=$HOME/miniconda3/bin
+  [[ -d "$condabin" ]] \
+    || die "Expected conda bin not found: $condabin"
+  log "Adding conda bin $condabin to path"
+  export PATH="$condabin:$PATH"
+  conda update -n base -y conda
+  conda init fish
+  conda init bash
+  conda config --add channels conda-forge
+  log "Installing mamba"
+  # https://docs.conda.io/projects/conda/en/latest/user-guide/tasks/manage-channels.html#strict-channel-priority
+  # conda config --set channel_priority false
+  conda install -y conda-libmamba-solver
+  conda install -y -c conda-forge mamba
+  additional_install="\
+    jupyter \
+    jupyterlab \
+    nb_conda_kernels \
+    pdm\
+  "
+  log "Installing via mamba:\n$additional_install"
+  mamba install -y -c conda-forge $additional_install
+else
+  "Skip conda install"
+fi
 
 log "PDM setup"
 pdm --pep582 >> ~/.bashrc
@@ -189,17 +202,18 @@ log "Emacs setup"
 if grep -q "START inserted by install script" ~/.bashrc; then
   log "Already found .bashrc modification!"
 else
+  log "Bashrc setup"
   # 'EOF' to prevent expansion
   cat >> ~/.bashrc << 'EOF'
 
 # START inserted by install script {{{
 
 if [ -d "$HOME/go/bin" ]; then
-  PATH="$HOME/go/bin:$PATH"
+  export PATH="$HOME/go/bin:$PATH"
 fi
 
 if [ -d "$HOME/.cargo/bin" ]; then
-  PATH="$HOME/.cargo/bin:$PATH"
+  export PATH="$HOME/.cargo/bin:$PATH"
 fi
 
 # Set rg and allow multi-file match
